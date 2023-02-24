5e4061b982d3f8d404f96c301a09ec88fd5bacef
elsa-kim <ksh96611@naver.com>
2023-02-25 00:23:21 +0900

    2/25

---
+++ note.js
@@ -0,0 +1,5 @@
+// The 2023 Frontend Development Crash Course - Learn HTML & CSS
+
+// https://www.youtube.com/watch?v=Kl3nOXQjVnQ
+
+// Figma : https://www.figma.com/community/file/1192839712389547623
